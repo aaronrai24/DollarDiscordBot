19b4fba36fbcc3bdc45c8be2dc812c17d396213c
arai1999 <arai1999@hotmail.com>
2023-02-25 22:08:35 -0800

    Added playlist loading, lyrics obtainabilltiy from Genius.com, as well as more QoL

--- music.py
+++ music.py
@@ -1,20 +1,24 @@
 #This music bot uses wavelink instead of FFMPEG
 #Current commands:
-#join, leave, play, skip, pause, resume, seek, volume, playskip, next
+#join, leave, play, skip, pause, resume, nowplaying, seek, volume, playskip, next, load, lyrics, stop
 import discord
 import os
 import wavelink
 import logging
 import asyncio
 import logging.handlers
+import random
 
+from pandas import *
 from discord.ext import commands
 from discord.utils import get
 from dotenv import load_dotenv
+from lyricsgenius import Genius
 
 # Role Constants
 ADMIN = "⚡️"
 DJ = "🎧"
+run = True
 
 #Create Unfiltered Bot to accept commands from other bots
 class UnfilteredBot(commands.Bot):
@@ -45,8 +49,9 @@ handler = logging.handlers.RotatingFileHandler(
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-#Get Discord token
+#Get Discord and Genius token from ENV
 DISCORD_TOKEN = os.getenv("TOKEN")
+genius = Genius("GENIUSTOKEN")
 
 #Wavelink setup
 @client.event
@@ -82,7 +87,6 @@ async def on_wavelink_track_end(player: CustomPlayer, track: wavelink.Track, rea
         logger.info(f'Playing next track: {next_track}')
     else:
         logger.info(f'Queue is empty')
-        #await client.change_presence(activity=discord.Game(name="DM me to see my commands"))
 
 #Scan messages to ensure message was sent in #commands chat
 @client.event
@@ -92,16 +96,28 @@ async def on_message(message):
     author = message.author
     
     if isinstance(message.channel, discord.channel.DMChannel) and message.author != client.user:
-        text = 'Current commands:\n\n!join  - Bot joins voice channel you are currently in \n!leave  - Bot leaves voice channel\n!play (Song)  - Plays desired song from YouTube Music\n!playsc (Song) - Plays desired song from SoundCloud\n!skip  - Skips current song\n!pause  - Pauses currently playing song\n!resume  - Resumes last played song \n!seek (int value)  - Fast forward or backtrack current play song volume\n!playskip (Song)  - Skips current playing song and plays song you chose from YouTube Music\n!next  - Shows next song in queue\n!queue - Prints out all items in queue'
+        text = 'All of my commands are listed in the #commands chat in the mfDiscord, too add me to your discord or to get a list of commands, DM Cash#8915!'
         await message.channel.send(text)
         logger.info(f'{author} sent a DM to Dollar')
-    
+ 
+
     if channel == 'commands' or channel == 'test':
         if msg.startswith('!'):
-            logger.info(f'Bot command entered; command: {msg}, author: {author}')
+            logger.info(f'Bot command entered. Command: {msg} | Author: {author}')
             await client.process_commands(message)
+        elif str(message.attachments) == "[]":
+            await client.process_commands(message)
+            logger.info(f'User message entered. Message: {msg} | Author: {author}')
         else:
-            logger.info(f'User message entered; message: {msg}, author: {author}')
+            split_v1 = str(message.attachments).split("filename='")[1]
+            filename = str(split_v1).split("' ")[0]
+            if filename.endswith(".csv"):                                   # refactor if possible
+                await message.attachments[0].save(fp='ex.csv')
+                await message.channel.send('File Downloaded, use !load to load songs into queue.')
+            logger.info(f'CSV successfully downloaded, author: {author}')
+    elif msg.startswith('!clear'):
+        await client.process_commands(message)
+        logger.info(f"{author} used !clear")
     elif msg.startswith('!'):
         logger.info(f'Command entered in wrong channel, deleting: {msg}')
         await message.delete(delay = 1)
@@ -112,13 +128,16 @@ async def on_message(message):
 async def on_voice_state_update(member, before, after):
     ctxbefore = before.channel
     ctxafter = after.channel
-    role = get(member.guild.roles, name="🎧")
+    role = get(member.guild.roles, name=DJ)
+
     if ctxbefore is None and ctxafter is not None:
         await member.add_roles(role)
         logger.info(f"{member} joined {ctxafter} adding Dj role")
     elif ctxbefore is not None and ctxafter is None:
         await member.remove_roles(role)
         logger.info(f"{member} left {ctxbefore} removing Dj role")
+    else:
+        logger.debug(f"{member} joined or left! Error adding or removing role")
 
 #Join authors voice channel
 @client.command()
@@ -257,7 +276,7 @@ async def pause(ctx):
         if vc.is_playing() and not vc.is_paused():
             await vc.pause()
             await ctx.send("Paused!")
-            logger.info(f'Pausing music')
+            logger.info('Pausing music')
         else:
             await ctx.send("Nothing is currently playing")
     else:
@@ -272,9 +291,24 @@ async def resume(ctx):
         if vc.is_paused():
             await vc.resume()
             await ctx.send("Resuming!")
-            logger.info(f'Resuming music')
+            logger.info('Resuming music')
         else:
             await ctx.send("Nothing is currently paused.")
+    else:
+        await ctx.send("The bot is not connected to a voice channel")
+
+#Show current playing song, ex: !nowplaying
+@client.command(aliases=['np'])
+@commands.has_role(DJ)
+async def nowplaying(ctx):
+    vc = ctx.voice_client
+    if vc:
+        try:
+            track = str(vc.track)
+            await ctx.send(f'Currently playing: {track}')
+            logger.info(f'Current playing track: {track}')
+        except:
+            await ctx.send(f'Nothing is currently playing, add a song by using !play or !playsc')
     else:
         await ctx.send("The bot is not connected to a voice channel")
 
@@ -288,6 +322,7 @@ async def next(ctx):
             search = vc.queue.get()
             vc.queue.put_at_front(item=search)
             await ctx.send(f"The next song is: {search}")
+            logger.info('Printing next song in queue')
         except:    
             await ctx.send("The queue is empty, add a song by using !play or !playsc")
     else:
@@ -302,7 +337,8 @@ async def seek(ctx, seek = 0):
     if vc:
         if vc.is_playing() and not vc.is_paused():
             await vc.seek(vc.track.length * val)
-            await ctx.send(f"Seeking!")
+            await ctx.send(f"Seeking {val} seconds.")
+            logger.info(f'Song seeked for {val} seconds')
         else:
             await ctx.send("Nothing is currently playing")
     else:
@@ -321,23 +357,27 @@ async def volume(ctx, volume):
     else:
         await ctx.send("The bot is not connected to a voice channel.")
 
-#Make copy of queue, get items and print to showcase all items
+#Prints all items in queue
 @client.command()
 @commands.has_role(DJ)
 async def queue(ctx):
+    global run
+    run = True
     vc = ctx.voice_client
     if vc.queue.is_empty is False:
-        logger.info(f'Printing Queue')    
+        logger.info('Printing Queue')    
         await ctx.send("Current Songs in queue: ")
         count = 1
         test = vc.queue.copy()
         while not test.is_empty:
+            if not run:
+                break
             item = test.get()
             await ctx.send(f"{count}: {item}")
             await asyncio.sleep(0.5)
             count += 1
     else:
-        logger.info(f'Queue is already empty')
+        logger.info('Queue is already empty')
         await ctx.send(f"The queue is currently empty, add a song by using !play or !playsc")
 
 #Make copy of queue, get items and print to showcase all items
@@ -366,6 +406,89 @@ async def clear(ctx, amount=None):
             await ctx.send("You must enter a number greater than 0")
         else:
             await ctx.channel.purge(limit=val)
+
+#Load playlist from CSV, ex !load
+@client.command()
+@commands.has_role(DJ)
+async def load(ctx):
+    vc = ctx.voice_client
+    global run
+    run = True
+    count = 0 
+
+    if not vc:
+        custom_player = CustomPlayer()
+        vc: CustomPlayer = await ctx.author.voice.channel.connect(cls=custom_player)
+        await vc.set_volume(5)
+
+    await ctx.send('Loading your playlist!')
+    logger.info('Loading Playlist')
+
+    data = read_csv("ex.csv")
+    tracks = data['Track Name'].tolist()
+    artists = data['Artist Name(s)'].tolist()
+    song = list(zip(tracks, artists))
+    
+    while count<=75:    
+        if not run:
+            break
+        item = random.choice(song)
+        song.remove(item)
+        search = await wavelink.YouTubeTrack.search(query=item[0] + " " + item[1], return_first=True)
+        if vc.is_playing():
+            vc.queue.put(item=search)
+            embed = discord.Embed(title=search.title, url=search.uri, description=f"Added {search.title} to the Queue!", colour=discord.Colour.random())
+            embed.set_author(name=f"{search.author}")
+            embed.set_thumbnail(url=f"{search.thumbnail}")
+            nextitem = vc.queue.get()
+            vc.queue.put_at_front(item=nextitem)
+            embed.set_footer(text=f"Next song is: {nextitem}")
+            await ctx.send(embed=embed)
+            logger.info(f'Added {search} to queue from playlist')
+        elif vc.queue.is_empty:
+            embed = discord.Embed(title=search.title, url=search.uri, description=f"Now Playing {search.title}!", colour=discord.Colour.random())
+            embed.set_author(name=f"{search.author}")
+            embed.set_thumbnail(url=f"{search.thumbnail}")
+            await ctx.send(embed=embed)
+            await vc.play(search)
+            logger.info(f'Playing {search} from playlist')
+        else:
+            logger.error('Error queuing/playing from playlist')
+        count += 1
+    
+    await ctx.send(f'Finished loading playlist')
+    logger.info(f'Finished loading {count} songs into queue from playlist')
+
+#Print lyrics of current playing song, pulls from Genius.com
+@client.command()
+@commands.has_role(DJ)
+async def lyrics(ctx):
+    vc = ctx.voice_client
+    track = str(vc.track)
+    
+    async with ctx.typing():
+        try:
+            song = genius.search_song(track)
+            logger.info('Lyrics loaded from Genius API')
+            if len(song.lyrics) > 4096:
+                return await ctx.send(f"Lyrics can be found here: <{song.url}>")
+            embed = discord.Embed(title=song.title, url=song.url, description=song.lyrics, colour=discord.Colour.random())
+            embed.set_author(name=f"{song.artist}")
+            embed.set_thumbnail(url=f"{song.header_image_thumbnail_url}")
+            embed.set_footer()
+            await ctx.send(embed=embed)
+        except TimeoutError:
+            ctx.send('Unable to get lyrics from Genius.com!')
+            logger.error('API Timeout/Unable to retrieve lyrics')
+
+#Stop loading playlist or printing queue
+@client.command()
+@commands.has_role(ADMIN or 'MOD')
+async def stop(ctx):
+    global run
+    run = False
+    logger.debug('Playlist/Queue loading interrupted')
+    await ctx.send('Interrupting!')
 
 #Error Handling if unable to find song, or user isn't in a voice channel
 async def play_error(ctx, error):
