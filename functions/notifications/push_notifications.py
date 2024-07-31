de71262be5114e3e33d20657398388792424a959
arai1999 <arai1999@hotmail.com>
2024-07-30 21:30:32 -0700

    initial commit

--- functions/notifications/push_notifications.py
+++ functions/notifications/push_notifications.py
@@ -33,5 +33,40 @@ class PushNotifications(lib.commands.Cog):
 			logger.info(f"Sending patch note notification for user: {user}")
 			await thread.send(f"<@{user}>, {game_name} has had a new update, check it out!")
 
+	def get_commute_duration(self, member):
+		"""
+		DESCRIPTION: Gets the commute duration
+		PARAMETERS: member (discord.Member) - Discord Member
+		RETURN: duration (int) - Duration of commute
+		"""
+		home_address = Queries.get_users_home_address(self.mydb, member.id)
+		work_address = Queries.get_users_work_address(self.mydb, member.id)
+
+		google_maps_api_key = lib.os.getenv('GOOGLE_MAPS_API_KEY')
+		gmaps = googlemaps.Client(key=google_maps_api_key)
+
+		directions = gmaps.directions(home_address, work_address)
+		first_leg = directions[0]['legs'][0]
+		duration = first_leg['duration']['value']
+
+		return duration
+	
+	async def notify_eta_to_work(self, member):
+		"""
+		DESCRIPTION: Notifies user of the time until work
+		PARAMETERS: member (discord.Member) - Discord Member
+		"""
+		logger.debug(f"Notifying {member} of their ETA to work...")
+		duration = get_commute_duration(member)
+		now = lib.datetime.datetime.now()
+		arrival_time = (now + duration).strftime("%I:%M %p")
+		departure_time = (now - duration).strftime("%I:%M %p")
+
+		message = f"""Good morning!\n\n
+		Estimated commute time from home to work at 8am is {duration}.\n
+		You should leave by {departure_time} to arrive at work by {arrival_time}.\n"""
+
+		GeneralFunctions.send_discord_dm(member, message)
+
 async def setup(bot):
 	await bot.add_cog(PushNotifications(bot))
